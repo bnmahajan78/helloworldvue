23f26137efa26a1fa26b4e536fa2ae790f55a395
bnmahajan78 <bnmahajan78@gmail.com>
2022-05-09 23:22:46 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <img alt="Vue logo" src="./assets/logo.png">
+
+    <HelloWorld msg="Place holder for component 1" />
+
+    <ImagePic msg="Place holder for component 2" />
+
+</template>
+
+<script>
+    import HelloWorld from './components/HelloWorld.vue'
+    import ImagePic from './components/ImagePic.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+      HelloWorld,
+      ImagePic
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
